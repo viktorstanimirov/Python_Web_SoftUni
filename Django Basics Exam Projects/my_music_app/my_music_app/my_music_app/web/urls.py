1322bb1e76200692bbb6fd71ca131acea618bda2
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-02-29 14:45:33 +0200

    Added 'few templates and some changes to setings'

--- Django Basics Exam Projects/my_music_app/my_music_app/my_music_app/web/urls.py
+++ Django Basics Exam Projects/my_music_app/my_music_app/my_music_app/web/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from my_music_app.web.views import index, create_profile
+from my_music_app.web.views import index
 
 urlpatterns = (
     path("", index, name="index"),
-    path("create-profile/", create_profile, name="create_profile"),
+
 )
--- Django Basics Exam Projects/my_music_app/my_music_app/my_music_app/web/views.py
+++ Django Basics Exam Projects/my_music_app/my_music_app/my_music_app/web/views.py
@@ -1,34 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
-from my_music_app.albums.models import Album
 from my_music_app.common.get_profile import get_profile
-
-from my_music_app.web.forms import CreateProfileForm
-
-
-def create_profile(request):
-    form = CreateProfileForm(request.POST or None)
-
-    if form.is_valid():
-        form.save()
-        return redirect("index")
-
-    context = {
-        "form": form,
-        "no_nav": True,
-    }
-
-    return render(request, "web/home-no-profile.html", context)
 
 
 def index(request):
     profile = get_profile()
 
     if profile is None:
-        return create_profile(request)
+        return render(request, "web/home-no-profile.html")
 
-    context = {
-        "albums": Album.objects.all(),
-    }
+    return render(request, "web/home-with-profile.html")
 
-    return render(request, "web/home-with-profile.html", context)
+
+def create_profile(request):
+    pass
